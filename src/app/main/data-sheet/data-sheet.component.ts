1ae39525c7124a8e2409aa730e0f787df41d6771
nu pan <135819902+unknown-nk@users.noreply.github.com>
2023-06-26 17:49:35 +0800

    feat: pages

--- src/app/main/data-sheet/data-sheet.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-data-sheet',
-  templateUrl: './data-sheet.component.html',
-  styleUrls: ['./data-sheet.component.scss'],
-})
-export class DataSheetComponent {}
